665b9581332788526c5d37c948d580feafd0ec91
aman <beyond.http@gmail.com>
2021-12-07 15:05:47 +0600

    theme vuex

---
+++ src/store/modules/theme.js
@@ -0,0 +1,23 @@
+export default {
+  actions: {
+    updateTheme({ commit, getters, dispatch }) {
+      commit('SET_DARK_THEME', !getters.IS_DARK_THEME)
+      dispatch('initTheme')
+    },
+    initTheme({ getters }) {
+      document.documentElement.className = getters.IS_DARK_THEME ? 'dark' : 'light'
+    }
+  },
+  mutations: {
+    SET_DARK_THEME: (state, isDarkTheme) => {
+      state.isDarkTheme = isDarkTheme
+    },
+  },
+  state: {
+    isDarkTheme: false
+  },
+  getters: {
+    IS_DARK_THEME: (state) => state.isDarkTheme,
+    GET_LOGO: (state) => state.isDarkTheme ? "https://aman3d.pythonanywhere.com/logo_dark.svg" : "https://aman3d.pythonanywhere.com/logo_light.svg"
+  }
+}
