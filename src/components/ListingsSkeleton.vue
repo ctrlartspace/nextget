3cb67166f5f3c5b4616132fb81f60d88fb94b822
aman <beyond.http@gmail.com>
2021-12-28 11:45:10 +0600

    add skeleton animation

---
+++ src/components/ListingsSkeleton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="listing skeleton" v-for="index in 1" :key="index">
+    <div class="offset-2px"></div>
+    <div class="row gx-2 d-flex justify-content-between">
+      <div class="col-auto">
+        <div
+          class="image box-69 d-flex align-items-center justify-content-center"
+        ></div>
+      </div>
+      <div class="col ellipsis">
+        <div class="title" />
+        <div class="offset-2px"></div>
+        <div class="desc" />
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListingsSkeleton",
+};
+</script>
+
+<style scoped>
+</style>
