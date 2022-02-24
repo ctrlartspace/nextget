265073a3e0919c223d2a31e62e022c287d3680a7
Aman Kirgizbayev [Shm] <AKirgizbayev@petrokazakhstan.com>
2022-02-24 15:02:47 +0600

    ui

---
+++ src/components/ListingsMini.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="listing" v-for="listing in listings" :key="listing.id">
+    <div class="row gx-2 d-flex justify-content-between">
+      <div class="col-auto">
+        <div
+          class="image box-36 d-flex align-items-center justify-content-center"
+          :class="{ 'with-border': listing.images.length == 0 }"
+        >
+          <img
+            v-if="listing.images.length > 0"
+            :src="`https://aman3d.pythonanywhere.com/uploads/listings/${listing.id}/thumbnail_${listing.images[0].id}`"
+            alt=""
+          />
+          <div v-else class="transparent secondary-text text-large">
+            <span class="material-icons-round">no_photography</span>
+          </div>
+        </div>
+      </div>
+      <div class="col ellipsis">
+        <router-link :to="{ name: 'Item', params: { id: listing.id } }">
+          {{
+            listing.product.name +
+            ", " +
+            listing.storage.value +
+            " GB, " +
+            listing.color.value
+          }}
+        </router-link>
+        <div class="offset-2px"></div>
+        <div class="d-inline">
+          <p>
+            <span class="accent-text">
+              <strong>
+                {{ numberWithCommas(listing.price) + " KZT " }}
+              </strong>
+            </span>
+            <span class="date secondary-text">
+              {{ fromNow(listing.created_date) }}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import conditionDecoder from "@/services/condition-decoder";
+
+export default {
+  name: "ListingsMini",
+  mixins: [conditionDecoder],
+  props: {
+    listings: {
+      type: Array,
+    },
+  },
+  methods: {
+    getConditionColor(condition) {
+      switch (condition) {
+        case 1:
+          return "condition10";
+        case 2:
+          return "condition9";
+        case 3:
+          return "condition8";
+        case 4:
+          return "condition7";
+        default:
+          return "condition9";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
